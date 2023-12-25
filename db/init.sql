8cb2abc1be9b40562abb7c849239ec1d5169b3bd
Vladimir Moshchuk <ardonplay@Vladimirs-iMac.local>
2023-12-26 01:08:54 +0300

    feat: full modify database

--- db/init.sql
+++ db/init.sql
@@ -1,35 +1,59 @@
-CREATE TABLE "user_role" (
-  "id" serial PRIMARY KEY,
-  "name" varchar UNIQUE NOT NULL
+CREATE TABLE
+    "user_role" (
+        "id" serial PRIMARY KEY,
+        "name" varchar UNIQUE NOT NULL
+    );
+
+CREATE TABLE
+    "page_type" (
+        "id" serial PRIMARY KEY,
+        "name" varchar UNIQUE NOT NULL
+    );
+
+CREATE TABLE
+    "page_element_type" (
+        "id" serial PRIMARY KEY,
+        "name" varchar UNIQUE NOT NULL
+    );
+
+CREATE TABLE
+    "users" (
+        "id" serial PRIMARY KEY,
+        "username" varchar UNIQUE NOT NULL,
+        "role" int NOT NULL,
+        "pass" varchar NOT NULL,
+        FOREIGN KEY ("role") REFERENCES "user_role" ("id") ON DELETE CASCADE
+    );
+
+CREATE TABLE
+    "pages" (
+        "id" serial PRIMARY KEY,
+        "parent_id" int,
+        "title" varchar NOT NULL,
+        "type" int NOT NULL,
+        FOREIGN KEY ("type") REFERENCES "page_type" ("id") ON DELETE CASCADE,
+        FOREIGN KEY ("parent_id") REFERENCES "pages" ("id") ON DELETE CASCADE
+    );
+
+CREATE TABLE
+    "page_content" (
+        "id" serial PRIMARY KEY,
+        "page_id" int NOT NULL,
+        "element_type" int NOT NULL,
+        "body" jsonb NOT NULL,
+        FOREIGN KEY ("page_id") REFERENCES "pages" ("id") ON DELETE CASCADE,
+        FOREIGN KEY ("element_type") REFERENCES "page_element_type" ("id") ON DELETE CASCADE
+
 );
 
-CREATE TABLE "page_type" (
-  "id" serial PRIMARY KEY,
-  "name" varchar UNIQUE NOT NULL
-);
+INSERT INTO "page_type" ("name") VALUES ('FOLDER'), ('PAGE');
 
-CREATE TABLE "page_element_type" (
-  "id" serial PRIMARY KEY,
-  "name" varchar UNIQUE NOT NULL
-);
+INSERT INTO "page_element_type" ("name") VALUES ('IMAGE'), ('TEXT');
 
+INSERT INTO "user_role" ("name") VALUES ('ROLE_ADMIN');
 
-CREATE TABLE "users" (
-  "id" serial PRIMARY KEY,
-  "username" varchar UNIQUE NOT NULL,
-  "role" int NOT NULL REFERENCES "user_role" ("id"),
-  "pass" varchar NOT NULL
-);
+INSERT INTO
+    "pages" ("parent_id", "title", "type")
+VALUES (NULL, 'TEST_FOLDER', 1), (1, 'TEST_PAGE', 2);
 
-CREATE TABLE "pages" (
-  "id" serial PRIMARY KEY,
-  "title" varchar NOT NULL,
-  "type" int NOT NULL REFERENCES "page_type" ("id")
-);
-
-CREATE TABLE "page_content" (
-  "id" serial PRIMARY KEY,
-  "page_id" int NOT NULL REFERENCES "pages" ("id"),
-  "element_type" int NOT NULL REFERENCES "page_element_type" ("id"),
-  "body" jsonb NOT NULL
-);
+INSERT INTO "page_content" ("page_id", "element_type", "body") VALUES (2, 2, '{"content": "Hello, this is test"}'::jsonb);
